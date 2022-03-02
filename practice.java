51fe0107aee20224271bb3a7d84f15be67622599
Parisheelan Negi <parisheelannegi@gmail.com>
2022-03-02 23:10:05 +0530

    java

--- practice.java
+++ practice.java
@@ -1,5 +1,8 @@
 public class practice {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.print("Hello"); // prints line
+        System.out.println("World"); // prints line with line break(\n)
+        System.out.println("Hello \t World"); // prints line with tab
+        System.out.println("\"World\""); // prints string with double quotes
     }
 }
